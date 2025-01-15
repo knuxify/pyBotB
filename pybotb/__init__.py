97594acfa357acb4a92e9d934949a5392bc9ceab
knuxify <knuxify@gmail.com>
2025-01-15 20:27:58 +0100

    init: fix circular import

--- pybotb/__init__.py
+++ pybotb/__init__.py
@@ -4,7 +4,7 @@ pybotb - Python library for interacting with the Battle of the Bits API and
 writing bots.
 """
 
-from .botb import BotB as BotB, Condition as Condition
-
 VERSION = "0.0.1"
 VERSION_TUPLE = (0, 0, 1)
+
+from .botb import BotB as BotB, Condition as Condition  # ignore: E402
